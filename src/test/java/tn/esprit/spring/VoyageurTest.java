aea73d44cd6edcffc010f4714f0a98f328833a65
zahra chouchane <zahracarthage.chouchene@esprit.tn>
2022-12-11 21:15:33 +0100

    mocktest

---
+++ src/test/java/tn/esprit/spring/VoyageurTest.java
@@ -0,0 +1,55 @@
+package tn.esprit.spring;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.Order;
+import tn.esprit.spring.entities.Voyageur;
+import tn.esprit.spring.repository.VoyageRepository;
+import tn.esprit.spring.services.VoyageServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class VoyageurTest {
+
+    @Mock
+    VoyageRepository voyageurRepository;
+
+    @InjectMocks
+    VoyageServiceImpl voyageurService;
+
+    Voyageur voyageur = new Voyageur();
+
+
+
+    @Test
+    @Order(0)
+    void addVoyageurTest()
+    {
+       // Voyageur voyageur = new Voyageur();
+        voyageur.setIdVoyageur(1L);
+        voyageur.setNomVoyageur("zahra");
+        verify(voyageurRepository, times(1)).save(voyageur);
+
+
+    }
+
+
+
+
+}
